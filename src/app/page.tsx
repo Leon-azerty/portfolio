582e16e90c92c4368b17706aae72f001487b9dd6
leon3108 <maxime.noel@epitech.eu>
2024-01-07 16:30:14 +0100

    feat: theme provider works with shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from './ModeToggle';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Hello world</p>
+      <ModeToggle />
     </main>
-  )
+  );
 }
