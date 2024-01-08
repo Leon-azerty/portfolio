9c4b8d05f138be543fa595f257994ae06c64e109
leon3108 <maxime.noel@epitech.eu>
2024-01-08 14:18:28 +0100

    Add @hookform/resolvers, @radix-ui/react-label, react-hook-form, and zod dependencies, and create Label and Input components

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -1,9 +1,101 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  sujet: z.string().min(2).max(50),
+  email: z.string().min(2).max(50),
+  message: z.string().min(2).max(50),
+});
+
 export default function Contact() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <section className="mt-20">
-      <form>
-        <p className="text-3xl">Contact Me</p>
-      </form>
+      <p className="text-3xl">Contact Me</p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} />
+                </FormControl>
+                {/* <FormDescription>This is your entreprise name.</FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="sujet"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Sujet</FormLabel>
+                <FormControl>
+                  <Input placeholder="placeholder" {...field} />
+                </FormControl>
+                {/* <FormDescription>This is your public display name.</FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>email</FormLabel>
+                <FormControl>
+                  <Input placeholder="example@gmail.com" {...field} />
+                </FormControl>
+                {/* <FormDescription>This is your public display name.</FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Message</FormLabel>
+                <FormControl>
+                  <Input placeholder="placeholder" {...field} />
+                </FormControl>
+                {/* <FormDescription>This is your public display name.</FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex flex-row-reverse">
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </Form>
     </section>
   );
 }
