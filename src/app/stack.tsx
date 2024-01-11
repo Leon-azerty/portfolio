7e491603b6688faf0a7febef96e528373f7f5e3a
leon3108 <maxime.noel@epitech.eu>
2024-01-11 13:38:04 +0000

    Refactor Element component and add descriptions

--- src/app/stack.tsx
+++ src/app/stack.tsx
@@ -6,12 +6,35 @@ import tailwindLogo from '@/public/logo_tailwind.png';
 import supabaseLogo from '@/public/logo_supabase.png';
 import postgresLogo from '@/public/logo_postgresql.png';
 
-function Element({ href, alt, src }: { href: string; alt: string; src: StaticImageData }) {
+const descriptions = {
+  react:
+    'JavaScript library for building dynamic and interactive user interfaces using a component-based architecture for reusability and efficient updates.',
+  nextjs:
+    'Framework for building fast, server-rendered React applications with built-in features like automatic code splitting and server-side rendering.',
+  typescript:
+    'Statically typed superset of JavaScript, enhancing code reliability by enabling type checking during development.',
+  tailwind: 'Framework that provides a set of pre-defined utility classes for styling without writing custom CSS.',
+  supabase:
+    'Platform that simplifies backend development, offering features like authentication, real-time data synchronization, and database management, all while leveraging PostgreSQL.',
+  postgres:
+    'Relational database management system widely used for data storage, offering advanced features, ACID compliance, and strong support for complex data types.',
+};
+
+function Element({
+  href,
+  alt,
+  src,
+  description,
+}: {
+  href: string;
+  alt: string;
+  src: StaticImageData;
+  description: string;
+}) {
   return (
-    <a href={href} target="_blank" className="z-30">
-      {/* <div className="relative w-[90px] h-[90px] sm:w-32 sm:h-32 md:w-4 md:h-40 md:mx-4 lg:w-4 lg:h-48 lg:mx-6"> */}
-      <Image alt={alt} src={src} />
-      {/* </div> */}
+    <a href={href} target="_blank" className="z-30 flex flex-col items-center 2xl:flex-row 2xl:mb-4">
+      <Image alt={alt} src={src} className="2xl:w-28" />
+      <p className="hidden lg:block text-justify mt-4 2xl:ml-2 2xl:mt-0 2xl:text-center">{description}</p>
     </a>
   );
 }
@@ -20,12 +43,32 @@ export default function Stack() {
   return (
     <section className="mt-52 flex justify-center">
       <div className="grid grid-cols-3 grid-rows-2 gap-4 p-4">
-        <Element href="https://react.dev/" alt="logo of React" src={reactLogo} />
-        <Element href="https://nextjs.org/" alt="logo of NextJS" src={nextjsLogo} />
-        <Element href="https://www.typescriptlang.org/" alt="logo of Typescipt" src={typescriptLogo} />
-        <Element href="https://tailwindcss.com/" alt="logo of Tailwind" src={tailwindLogo} />
-        <Element href="https://supabase.com/" alt="logo of Supabase" src={supabaseLogo} />
-        <Element href="https://www.postgresql.org/" alt="logo of PostgreSQL" src={postgresLogo} />
+        <Element href="https://react.dev/" alt="logo of React" src={reactLogo} description={descriptions.react} />
+        <Element href="https://nextjs.org/" alt="logo of NextJS" src={nextjsLogo} description={descriptions.nextjs} />
+        <Element
+          href="https://www.typescriptlang.org/"
+          alt="logo of Typescipt"
+          src={typescriptLogo}
+          description={descriptions.typescript}
+        />
+        <Element
+          href="https://tailwindcss.com/"
+          alt="logo of Tailwind"
+          src={tailwindLogo}
+          description={descriptions.tailwind}
+        />
+        <Element
+          href="https://supabase.com/"
+          alt="logo of Supabase"
+          src={supabaseLogo}
+          description={descriptions.supabase}
+        />
+        <Element
+          href="https://www.postgresql.org/"
+          alt="logo of PostgreSQL"
+          src={postgresLogo}
+          description={descriptions.postgres}
+        />
       </div>
     </section>
   );
