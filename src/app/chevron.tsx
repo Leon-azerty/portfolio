286e8ed35fe86c9db66d2fad060db02a9d2df921
leon3108 <maxime.noel@epitech.eu>
2024-01-27 11:59:15 +0000

    Add bounce animation to chevron component

--- src/app/chevron.tsx
+++ src/app/chevron.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Chevron() {
   return (
-    <div className="hidden lg:flex w-full h-40 justify-around mt-96 2xl:mt-40">
+    <div className="hidden lg:flex w-full h-40 justify-around mt-96 2xl:mt-40 animate-bounce">
       <Image src="/chevron.png" alt="image of chevron" className="w-40 h-40" width={100} height={100} />
       <Image src="/chevron.png" alt="image of chevron" className="w-40 h-40" width={100} height={100} />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,11 +29,22 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "bounce": {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        }
       },
       animation: {
         "infinite-left-to-right": "left-to-right 6s infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "bounce": "bounce 3s infinite",
       },
       colors: {
         react: '#437AA1',
