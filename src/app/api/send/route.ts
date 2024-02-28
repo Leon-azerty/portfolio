223808bc8a2ef297246c653514a7dd21de7a5253
leon3108 <maxime.noel@epitech.eu>
2024-02-28 11:06:44 +0000

    pass email through var env

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,22 +1,8 @@
 import { EmailTemplate } from '@/components/emailTemplate';
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
-import * as z from "zod";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const fileSchema = z.object({
-  filename: z.string(),
-  content: z.instanceof(ArrayBuffer),
-});
-
-const sendRouteSchema = z.object({
-  name: z.string().min(2).max(50),
-  subject: z.string().min(2).max(50),
-  email: z.string().email().min(2).max(50),
-  message: z.string().min(20).max(2000),
-  attachments: z.array(fileSchema),
-});
 
 export async function POST(req: NextRequest) {
   try {
@@ -25,7 +11,7 @@ export async function POST(req: NextRequest) {
 
     const data = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['maxime.noel@epitech.eu'],
+      to: [process.env.EMAIL!],
       subject: 'Nouveau message de ' + name,
       react: EmailTemplate({ name, email, message, subject}),
       text: 'useless text ??',
