3d6a9130abcabffd5b965a294f4d3080a5f6b610
leon3108 <maxime.noel@epitech.eu>
2024-06-20 10:13:56 +0200

    feat: add sidebar context in provider

--- src/app/common/sidebar-provider.tsx
+++ src/app/common/sidebar-provider.tsx
@@ -1,8 +1,17 @@
 'use client';
 
-import { SidebarContext } from '@/contexts/sideBarContext';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
+
+type SidebarContextType = {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const SidebarContext = createContext<SidebarContextType>({
+  isOpen: false,
+  setIsOpen: () => {},
+});
 
 export function SideBarProvider({ children, ...props }: ThemeProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
