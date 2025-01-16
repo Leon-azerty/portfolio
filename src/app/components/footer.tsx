8dc513e637f5f348f4e16c10c9256a194b053d51
Leon-azerty <maxime.noel@epitech.eu>
2025-01-16 12:52:16 +0100

    feat: update footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,28 +2,46 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <section className="my-10">
-      <div className="flex justify-center">
-        <div className="border-divider w-[80%] border-t">&nbsp;</div>
+    <footer className="m-4 rounded-lg bg-white shadow dark:bg-slate-950">
+      <div className="mx-auto w-full max-w-screen-xl p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">Maxime NOEL</span>
+          <ul className="mb-6 flex flex-wrap items-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:mb-0">
+            <li>
+              <Link href="#about" className="me-4 hover:underline md:me-6">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="#projects" className="me-4 hover:underline md:me-6">
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link href="#internships" className="me-4 hover:underline md:me-6">
+                Internships
+              </Link>
+            </li>
+            <li>
+              <Link href="#experiences" className="me-4 hover:underline md:me-6">
+                Experiences
+              </Link>
+            </li>
+            <li>
+              <Link href="#contact" className="me-4 hover:underline md:me-6">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+        <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+          Made By Me with NextJs and Tailwind. See the repository on{' '}
+          <a className="underline" href="https://github.com/Leon-azerty/portfolio" target="_blank">
+            Github
+          </a>
+        </span>
       </div>
-      <div className="flex flex-wrap justify-center space-x-2 sm:space-x-4 md:space-x-8">
-        <Link href="#about">About</Link>
-        <Link href="#projects">Projects</Link>
-        <Link href="#internships">Internships</Link>
-        <Link href="#experiences">Experiences</Link>
-        <Link href="#contact">Contact</Link>
-      </div>
-      <p className="pt-3 text-center text-zinc-300">
-        Made By Me with NextJs and Tailwind
-        <br />
-        See the repository on{' '}
-        <a className="underline" href="https://github.com/Leon-azerty/portfolio" target="_blank">
-          Github
-        </a>
-        <br />
-        License MIT
-        <br />
-      </p>
-    </section>
+    </footer>
   );
 }
