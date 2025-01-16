6ac927df569a68afc356e24077c018b3eadaee38
Leon-azerty <maxime.noel@epitech.eu>
2025-01-16 18:27:02 +0100

    feat: add epitech internship

---
+++ src/app/components/Internships/epitech.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import epitech from '@/public/epitech.webp';
+import { Card, CardContent, CardHeader, CardTitle } from '@/ui/card';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import Image from 'next/image';
+import { useRef } from 'react';
+
+export default function Epitech() {
+  const container = useRef<HTMLElement>(null);
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const anim = gsap.fromTo(
+      container.current,
+      {
+        opacity: 0,
+        x: +100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        paused: true,
+      }
+    );
+
+    ScrollTrigger.create({
+      trigger: container.current,
+      start: 'center 90%',
+      onEnter: () => anim.play(),
+      onEnterBack: () => anim.play(),
+    });
+
+    ScrollTrigger.create({
+      trigger: container.current,
+      start: 'top bottom',
+      onLeave: () => anim.pause(0),
+      onLeaveBack: () => anim.pause(0),
+    });
+  });
+
+  return (
+    <section className="h-full" ref={container}>
+      <div className="flex items-center lg:hidden">
+        <Image
+          src={epitech}
+          alt="Epitech's Logo"
+          style={{
+            width: '90px',
+            height: 'auto',
+          }}
+        />
+        <p className="ml-2 text-2xl">Epitech</p>
+      </div>
+
+      <Card className="h-full">
+        <CardHeader className="hidden lg:block">
+          <CardTitle className="flex items-center">
+            <div className="flex">
+              <Image
+                src={epitech}
+                alt="Epitech's Logo"
+                style={{
+                  width: '90px',
+                  height: 'auto',
+                }}
+              />
+              <div className="ml-2">
+                <CardTitle className="hidden: lg:block">Epitech</CardTitle>
+              </div>
+            </div>
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          During my fifth year, I worked part-time at Epitech as an Educational Supervisor from September 2024 to March
+          2025. Throughout this experience, I developed my soft skills, learned to resolve interpersonal conflicts with
+          students, found solutions to help teams complete their projects, and provided technical assistance with their
+          code. I also participated in project evaluations, pedagogical follow-ups, and guided students through their
+          technological choices and architectural decisions.
+        </CardContent>
+      </Card>
+    </section>
+  );
+}
--- src/app/components/Internships/internships.tsx
+++ src/app/components/Internships/internships.tsx
@@ -1,17 +1,20 @@
+import Epitech from './epitech';
 import GnConsulting from './gnConsulting';
 import Piwio from './piwio';
 import Viapix from './viapix';
 
 export default function Internships() {
   return (
-    <section className="mt-20 relative z-20" id="internships">
+    <section className="z-20 mt-96 min-h-fit" id="internships">
       <p className="text-4xl">Internships</p>
-      <div className="lg:grid lg:grid-cols-2 lg:gap-4 overflow-hidden">
+      <div className="lg:grid lg:grid-cols-2 lg:gap-4">
         <Piwio />
         <div className="my-10 lg:hidden">&nbsp;</div>
         <Viapix />
         <div className="my-10 lg:hidden">&nbsp;</div>
         <GnConsulting />
+        <div className="my-10 lg:hidden">&nbsp;</div>
+        <Epitech />
       </div>
     </section>
   );
