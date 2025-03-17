6fb0efb437cb900cbe385069d1cab3d76b0d31dc
Leon-azerty <maxime.noel@epitech.eu>
2025-03-17 18:56:18 +0100

    feat: add projects in /projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,23 @@
+import Contact from '../components/contact';
+import Footer from '../components/footer';
 import Navbar from '../components/navbar';
+import Checked from '../components/Projects/checked';
+import Portfolio from '../components/Projects/portfolio';
+import SpriteCloud from '../components/Projects/spriteCloud';
 
 export default function Page() {
   return (
     <main className="px-4 md:px-20 lg:px-28 xl:px-32">
       <Navbar />
-      <p className="flex h-screen items-center justify-center text-2xl">projets</p>
+      <section id="projects" className="my-20 overflow-hidden lg:my-40">
+        <Checked />
+        <div className="my-10">&nbsp;</div>
+        <Portfolio />
+        <div className="my-10">&nbsp;</div>
+        <SpriteCloud />
+      </section>
+      <Contact />
+      <Footer />
     </main>
   );
 }
